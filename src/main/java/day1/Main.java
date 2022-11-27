8db223b0b36383ba01d9a4cf79735b56b1044b40
Eddie Zaneski <eddiezane@gmail.com>
2022-11-27 13:36:40 -0700

    Add day 1 part 2
    Signed-off-by: Eddie Zaneski <eddiezane@gmail.com>
    Co-Authored-By: PURPUR124 <eustolioea@gmail.com>

--- src/main/java/day1/Main.java
+++ src/main/java/day1/Main.java
@@ -26,16 +26,30 @@ public class Main {
             numList.add(Integer.valueOf(line));
         }
 
+        // taking the first three integers from the input, adding them, then repeating the process from the 2nd to the 4th
+        // integer and the comparing them like before
+        ArrayList<Integer> sums = new ArrayList<>();
+        Integer firstSum = numList.get(0) + numList.get(1) + numList.get(2);
+        sums.add(firstSum);
+
+        for (int i = 3; i < numList.size(); i++) {
+            Integer sum = numList.get(i) + numList.get(i - 1) + numList.get(i - 2);
+            sums.add(sum);
+        }
+
+        // sums
+        // takes the sums from 3 integers from numList
+
         // we have a list numbers
         // numList
         // take number(int1) and compare it to number(int2) and see if there's an increase between int1 and int2
         // start our count at 0
         int count = 0;
         // set last to the first number in the list
-        int last = numList.get(0);
+        int last = sums.get(0);
         // this looks at the last number and determines whether or not it is larger than the last number, and if
         // it was then it does not add to the count
-        for (Integer i : numList) {
+        for (Integer i : sums) {
             if (last < i) {
                 count = count + 1;
             }
